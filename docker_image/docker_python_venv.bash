2234c84a2341fd16fc101e564e5dd85aafd58196
Havlia <haavaali@stud.ntnu.no>
2026-04-28 18:12:02 +0200

    Drep mæ no kanskje? eller i morra, ka enn som funke.

--- docker_image/docker_python_venv.bash
+++ docker_image/docker_python_venv.bash
@@ -13,15 +13,3 @@ python3 -m "venv" "${BNL_VENV_PATH}/.venv" && source "${BNL_VENV_PATH}/.venv/bin
 		 				torch \
 		 				torchvision \
 		 				ultralytics-opencv-headless 
-
-git clone https://github.com/raspberrypi/libcamera.git
-
-cd libcamera
-
-git checkout v0.5.0+rpt20250429
-
-meson setup build
-
-sudo  ninja -C build
-
-sudo ninja -C build install
